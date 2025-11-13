71ae8c83bb5b2acf52f1d16cff4b29f64f8a9c92
Yeromin Dmytro <eremin.dmitriy@ukr.net>
2025-11-13 22:11:43 +0200

    gjh

--- vite.config.js
+++ vite.config.js
@@ -1,76 +1,24 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
-import fs from 'fs';
-import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-
 import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { resolve } from 'path';
 
-// Автоматичне збирання всіх HTML-файлів у src/partials/
-const getHtmlEntries = () => {
-  const pagesDir = resolve(__dirname, 'src/partials');
-  const entries = {};
-
-  const walk = dir => {
-    fs.readdirSync(dir).forEach(file => {
-      const fullPath = resolve(dir, file);
-      if (fs.statSync(fullPath).isDirectory()) {
-        walk(fullPath);
-      } else if (file.endsWith('.html')) {
-        const name = fullPath
-          .replace(pagesDir + '/', '')
-          .replace('.html', '')
-          .replace(/\//g, '-');
-        entries[name] = fullPath;
-      }
-    });
-  };
-
-  walk(pagesDir);
-  return entries;
-};
-
-export default defineConfig(({ command }) => {
-  const inputFiles = glob.sync('./src/**/*.html');
-
-  return {
-    define: {
-      [command === 'serve' ? 'global' : '_global']: {},
-    },
-
-    base: '/yeromin1.github.io/TradeBlade/',
-    root: 'src',
-    build: {
-      emptyOutDir: true,
-      sourcemap: true,
-      outDir: '../dist',
-      rollupOptions: {
-        input: {
-          main: resolve(__dirname, 'src/index.html'),
-          ...getHtmlEntries(),
-        },
-        output: {
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-              return 'vendor';
-            }
-          },
-          entryFileNames: 'commonHelpers.js',
-        },
+export default defineConfig({
+  base: '/TradeBlade/', // <-- имя репозитория
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'), // главный HTML
       },
     },
-    plugins: [
-      injectHTML(),
-      FullReload(['./src/**/*.html']),
-      viteStaticCopy({
-        targets: [
-          {
-            src: 'src/img/icons.svg',
-            dest: 'img',
-          },
-        ],
-      }),
-    ],
-  };
+  },
+  plugins: [
+    injectHTML(),
+    FullReload(['./src/**/*.html']),
+    viteStaticCopy({
+      targets: [{ src: 'src/img/icons.svg', dest: 'img' }],
+    }),
+  ],
 });
